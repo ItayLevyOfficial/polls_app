0264c48cc7c98dc53f792f43cc397000d85b801b
Itay Levy <itaylevy134@gmail.com>
2020-02-20 19:07:10 +0200

    finished until the admin section

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 
 class Question(models.Model):
-    question_txt = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField(verbose_name='data published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
@@ -11,3 +19,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_text

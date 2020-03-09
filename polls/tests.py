0126a3fd1e98a6c5e03326927db75b394ea68268
Itay Levy <itaylevy134@gmail.com>
2020-03-10 01:24:47 +0200

    added index test class and first test function

--- polls/tests.py
+++ polls/tests.py
@@ -1,7 +1,21 @@
 import datetime
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
 from .models import Question
+
+
+def create_question(question_text, days):
+    pub_date = timezone.now() + datetime.timedelta(days=days)
+    return Question(question_text=question_text, pub_date=pub_date)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEquals(first=200, second=response.status_code)
+        self.assertContains(response=response, text='No polls are available.', html=True)
+        self.assertQuerysetEqual([], response.context['latest_question_list'])
 
 
 class QuestionModelTests(TestCase):

3b2a51b2c2b54a38b1a26ebbea77568da7f3203c
Itay Levy <itaylevy134@gmail.com>
2020-03-09 21:12:56 +0200

    wrote more tests to the was_published_recently function

--- polls/tests.py
+++ polls/tests.py
@@ -6,7 +6,17 @@ from .models import Question
 
 class QuestionModelTests(TestCase):
 
+    def test_published_recently_with_recent_question(self):
+        pub_date = timezone.now() - datetime.timedelta(seconds=30)
+        recent_question = Question(pub_date=pub_date)
+        self.assertIs(expr1=True, expr2=recent_question.was_published_recently())
+
+    def test_published_recently_with_old_question(self):
+        pub_date = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=pub_date)
+        self.assertIs(expr1=False, expr2=old_question.was_published_recently())
+
     def test_published_recently_with_future_question(self):
-        publish_time = timezone.now() + datetime.timedelta(days=1)
-        future_question = Question(pub_date=publish_time)
+        pub_date = timezone.now() + datetime.timedelta(days=1)
+        future_question = Question(pub_date=pub_date)
         self.assertIs(expr1=False, expr2=future_question.was_published_recently())

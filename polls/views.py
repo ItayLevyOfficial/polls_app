5197639c8e3640ddb57e00dd09fc4cfe2188c54e
Itay Levy <itaylevy134@gmail.com>
2020-03-09 19:27:25 +0200

    Changed the index view to use generic view

--- polls/views.py
+++ polls/views.py
@@ -7,20 +7,22 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.db.models import F
+
 from .models import Question, Choice
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request=request, template_name='polls/index.html', context=context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    return render(request=request, template_name='polls/detail.html', context={'question': question})
 
 
 def results(request, question_id):
@@ -42,4 +44,3 @@ def vote(request, question_id):
         selected_choice.votes = F(name='votes') + 1
         selected_choice.save()
         return HttpResponseRedirect(redirect_to=reverse('polls:results', args=(question_id,)))
-

9d4db0476d61064b0fed429b82749d1a5db4aef1
Itay Levy <itaylevy134@gmail.com>
2020-03-09 18:16:15 +0200

    The vote count now updated using F

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse, HttpRequest, Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
-
+from django.db.models import F
 from .models import Question, Choice
 
 
@@ -24,8 +24,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(klass=Question, pk=question_id)
+    return render(request=request, template_name='polls/results.html', context={'question': question})
 
 
 def vote(request, question_id):
@@ -39,7 +39,7 @@ def vote(request, question_id):
             context={'question': question, 'error_message': 'You didn\'t select a choice.'}
         )
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F(name='votes') + 1
         selected_choice.save()
         return HttpResponseRedirect(redirect_to=reverse('polls:results', args=(question_id,)))
 

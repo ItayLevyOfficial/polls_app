f21cc89b3a352c18fc8a19f1b5389e07cf2b5fe3
Itay Levy <itaylevy134@gmail.com>
2020-03-12 00:52:03 +0200

    Finished until 'Customize the admin change list'

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -7,6 +11,7 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']})
     ]
+    inlines = [ChoiceInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
